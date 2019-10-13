8b13482de532a5289fa7d51fcea9b8454a27c6b4
Steven Lord <lords@oregonstate.edu>
2019-10-13 03:41:55 -0700

    Add trashCard fcn

--- projects/lords/dominion/dominion.c
+++ projects/lords/dominion/dominion.c
@@ -1329,7 +1329,31 @@ int discardCard(int handPos, int currentPlayer, struct gameState *state)
 
 int trashCard(int handPos, int currentPlayer, struct gameState *state)
 {
+    //set played card to -1
+    state->hand[currentPlayer][handPos] = -1;
 
+    //remove card from player's hand
+    if ( handPos == (state->handCount[currentPlayer] - 1) )     //last card in hand array is played
+    {
+        //reduce number of cards in hand
+        state->handCount[currentPlayer]--;
+    }
+    else if ( state->handCount[currentPlayer] == 1 ) //only one card in hand
+    {
+        //reduce number of cards in hand
+        state->handCount[currentPlayer]--;
+    }
+    else
+    {
+        //replace discarded card with last card in hand
+        state->hand[currentPlayer][handPos] = state->hand[currentPlayer][ (state->handCount[currentPlayer] - 1)];
+        //set last card to -1
+        state->hand[currentPlayer][state->handCount[currentPlayer] - 1] = -1;
+        //reduce number of cards in hand
+        state->handCount[currentPlayer]--;
+    }
+
+    return 0;
 }
 
 
