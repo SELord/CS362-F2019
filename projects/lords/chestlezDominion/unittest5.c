b631bdfe7fbed86879dd7207d3acea487bef28d8
Steven <lords@oregonstate.edu>
2019-11-24 03:11:49 -0800

    Add unit test for choice1 = silver

--- projects/lords/chestlezDominion/unittest5.c
+++ projects/lords/chestlezDominion/unittest5.c
@@ -56,7 +56,7 @@ int main(){
     memcpy(&control, &testState, sizeof(struct gameState));
 
 
-    r = playMine(choice1, choice2, handPos, &testState, p);
+    r = mineHandler(&testState, choice1, choice2, p);
 
 
     printf("Check for return code -1\n");
@@ -84,7 +84,7 @@ int main(){
     memcpy(&control, &testState, sizeof(struct gameState));
 
 
-    r = playMine(choice1, choice2, handPos, &testState, p);
+    r = mineHandler(&testState, choice1, choice2, p);
 
 
 
@@ -112,7 +112,7 @@ int main(){
     memcpy(&control, &testState, sizeof(struct gameState));
 
 
-    r = playMine(choice1, choice2, handPos, &testState, p);
+    r = mineHandler(&testState, choice1, choice2, p);
 
 
 
@@ -142,7 +142,78 @@ int main(){
     memcpy(&control, &testState, sizeof(struct gameState));
 
 
-    r = playMine(choice1, choice2, handPos, &testState, p);
+    r = mineHandler(&testState, choice1, choice2, p);
+
+    j = control.hand[p][choice1];
+
+    //gain new treasure card to hand
+    control.hand[p][control.handCount[p]] = choice2;
+    control.handCount[p]++;
+
+
+    //discard mine card from deck
+    control.discard[p][control.discardCount[p]] = control.hand[p][handPos];
+    control.discardCount[p]++;  //increment the count of cards in players discard pile
+    control.hand[p][handPos] = -1;
+    control.hand[p][handPos] = control.hand[p][ (control.handCount[p] - 1)];
+    control.hand[p][control.handCount[p] - 1] = -1;
+    control.handCount[p]--;
+
+
+    //trash the choice1 treasure card
+    for (i = 0; i < control.handCount[p]; i++)
+    {
+        if (control.hand[p][i] == j)
+        {
+            //trash card
+            control.hand[p][i] = -1;
+            control.hand[p][i] = control.hand[p][ (control.handCount[p] - 1)];
+            control.hand[p][control.handCount[p] - 1] = -1;
+            control.handCount[p]--;
+
+            break;
+        }
+    }
+
+
+
+    printf("Check for return code 0\n");
+    noAbortAssert(ptrR, &zero, sizeof(int));
+
+
+    printf("Check that treasure was added to hand\n");
+    noAbortAssert(&control.hand[p][i], &testState.discard[p][i], sizeof(int));
+
+
+    printf("Check that handCount is correct\n");
+    noAbortAssert(&control.handCount[p], &testState.handCount[p], sizeof(int));
+
+
+    printf("Check that mine was discarded\n");
+    noAbortAssert(&control.discard[p][ control.discardCount[p] ], &testState.discard[p][testState.discardCount[p]], sizeof(int));
+
+
+
+
+
+    printf("\n\n*****    TEST 5: Positive test case: choice1 == silver, choice2 == gold costing exactly 3 more    *****\n");
+
+
+    memcpy(&testState, &G, sizeof(struct gameState));
+
+    testState.hand[0][0] = mine;
+    testState.hand[0][1] = estate;
+    testState.hand[0][2] = estate;
+    testState.hand[0][3] = silver;
+    testState.hand[0][4] = copper;
+    handPos = 0;
+    choice1 = 3;  //choice1 is a treasure
+    choice2 = gold;  //choice2 is a treasure costing allowed
+
+    memcpy(&control, &testState, sizeof(struct gameState));
+
+
+    r = mineHandler(&testState, choice1, choice2, p);
 
     j = control.hand[p][choice1];
 
